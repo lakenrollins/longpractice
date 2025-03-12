22eae7c9b78667f64b8f3cf9f21bad7d2c623fcc
lakenrollins <lakenrollins@gmail.com>
2025-03-11 20:00:02 -0500

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
@@ -38,5 +41,11 @@ router.post('/test', function(req, res) {
     }
   );
 
-
+  router.use('/session', sessionRouter);
+  router.use('/users', usersRouter);
+  
+  router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+  });
+  
 module.exports = router;
